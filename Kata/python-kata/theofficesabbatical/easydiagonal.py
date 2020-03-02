6664f5c83103a10903c43fa0694459552fe13c30
Felix Roske <f.roske@roskenet.de>
2020-03-02 23:25:15 +0100

    Solve easydiagonal.py

--- Kata/python-kata/theofficesabbatical/easydiagonal.py
+++ Kata/python-kata/theofficesabbatical/easydiagonal.py
@@ -4,21 +4,19 @@ Created on Mar 1, 2020
 @author: felix
 '''
 
+import fractions
+
 def coeff(n, k):
     if(2 * k > n) :
         k = n - k
     
-    result = 1
+    result = fractions.Fraction(1)
     for i in range(1, k+1):
-        result = result * ((n - k + i) / i)
+        result = result * fractions.Fraction((n - k + i),i)
     
     return result
 
 def diagonal(n, p):
-    result = 0
-    for i in range(n, p-1, -1):
-        result = result + coeff(i, p)
-
-    return result
+    return coeff(n+1, p+1)
 
 print(diagonal(20, 5)) #Should be 54264
